0e7f27851f77edd7502638038966181d01ed8081
kamipown <paul.delobbel@gmail.com>
2016-04-01 03:24:32 +0200

    hsl inputs

--- ColorConverter/sources/js/app.js
+++ ColorConverter/sources/js/app.js
@@ -119,17 +119,101 @@ var App =
 
 	hex_r_text_change: function()
 	{
-
+		var n = parseInt(this.hex_r_text.value, 16);
+		if (n >= 0 && n <= 255)
+		{
+			this.hex_r_text.className = "";
+			this.hex_r_range.value = n;
+			this.update_hex();
+		}
+		else
+			this.hex_r_text.className = "incorrect";
 	},
 
 	hex_g_text_change: function()
 	{
-
+		var n = parseInt(this.hex_g_text.value, 16);
+		if (n >= 0 && n <= 255)
+		{
+			this.hex_g_text.className = "";
+			this.hex_g_range.value = n;
+			this.update_hex();
+		}
+		else
+			this.hex_g_text.className = "incorrect";
 	},
 
 	hex_b_text_change: function()
 	{
+		var n = parseInt(this.hex_b_text.value, 16);
+		if (n >= 0 && n <= 255)
+		{
+			this.hex_b_text.className = "";
+			this.hex_b_range.value = n;
+			this.update_hex();
+		}
+		else
+			this.hex_b_text.className = "incorrect";
+	},
 
+	hsl_h_range_change: function()
+	{
+		this.hsl_h_text.value = this.hsl_h_range.value;
+		this.hsl_h_text.className = "";
+		this.update_hsl();
+	},
+
+	hsl_s_range_change: function()
+	{
+		this.hsl_s_text.value = this.hsl_s_range.value;
+		this.hsl_s_text.className = "";
+		this.update_hsl();
+	},
+
+	hsl_l_range_change: function()
+	{
+		this.hsl_l_text.value = this.hsl_l_range.value;
+		this.hsl_l_text.className = "";
+		this.update_hsl();
+	},
+
+	hsl_h_text_change: function()
+	{
+		var n = Number(this.hsl_h_text.value);
+		if (n >= 0 && n <= 359 && this.hsl_h_text.value != "")
+		{
+			this.hsl_h_text.className = "";
+			this.hsl_h_range.value = n;
+			this.update_hsl();
+		}
+		else
+			this.hsl_h_text.className = "incorrect";
+	},
+
+	hsl_s_text_change: function()
+	{
+		var n = Number(this.hsl_s_text.value);
+		if (n >= 0 && n <= 100 && this.hsl_s_text.value != "")
+		{
+			this.hsl_s_text.className = "";
+			this.hsl_s_range.value = n;
+			this.update_hsl();
+		}
+		else
+			this.hsl_s_text.className = "incorrect";
+	},
+
+	hsl_l_text_change: function()
+	{
+		var n = Number(this.hsl_l_text.value);
+		if (n >= 0 && n <= 100 && this.hsl_l_text.value != "")
+		{
+			this.hsl_l_text.className = "";
+			this.hsl_l_range.value = n;
+			this.update_hsl();
+		}
+		else
+			this.hsl_l_text.className = "incorrect";
 	},
 
 	update_rgb: function()
@@ -148,6 +232,16 @@ var App =
 		{
 			this.update_rgb_from_hex();
 			this.update_hsl_from_rgb();
+			this.update_results();
+		}
+	},
+
+	update_hsl: function()
+	{
+		if (document.querySelectorAll("#hsl_section .incorrect").length == 0)
+		{
+			this.update_rgb_from_hsl();
+			this.update_hex_from_rgb();
 			this.update_results();
 		}
 	},
@@ -222,10 +316,78 @@ var App =
 		this.rgb_b_text.value = this.hex_b_range.value;
 	},
 
+	update_rgb_from_hsl: function()
+	{
+		var r, g, b, t1, t2, tr, tg, tb;
+		var h = parseInt(this.hsl_h_range.value) / 360;
+		var s = parseInt(this.hsl_s_range.value) / 100;
+		var l = parseInt(this.hsl_l_range.value) / 100;
+
+		if (s == 0)
+		{
+			r = g = b = this.round(l * 255);
+		}
+		else
+		{
+			if (l < 0.5)
+				t1 = l * (s + 1);
+			else
+				t1 = l + s - l * s;
+
+			t2 = l * 2 - t1;
+
+			tr = h + 0.333;
+			tg = h;
+			tb = h - 0.333;
+
+			if (tr < 0) ++tr;
+			if (tr > 1) -- tr;
+			if (tg < 0) ++tg;
+			if (tg > 1) -- tg;
+			if (tb < 0) ++tb;
+			if (tb > 1) -- tb;
+
+			if (tr * 6 < 1)
+				r = t2 + (t1 - t2) * tr * 6;
+			else if (tr * 2 < 1)
+				r = t1;
+			else if (tr * 3 < 2)
+				r = t2 + (t1 - t2) * (0.666 - tr) * 6;
+			else
+				r = t2;
+
+			if (tg * 6 < 1)
+				g = t2 + (t1 - t2) * tg * 6;
+			else if (tg * 2 < 1)
+				g = t1;
+			else if (tg * 3 < 2)
+				g = t2 + (t1 - t2) * (0.666 - tg) * 6;
+			else
+				g = t2;
+
+			if (tb * 6 < 1)
+				b = t2 + (t1 - t2) * tb * 6;
+			else if (tb * 2 < 1)
+				b = t1;
+			else if (tb * 3 < 2)
+				b = t2 + (t1 - t2) * (0.666 - tb) * 6;
+			else
+				b = t2;
+
+			r = this.round(r * 255);
+			g = this.round(g * 255);
+			b = this.round(b * 255);
+		}
+
+		this.rgb_r_text.value = this.rgb_r_range.value = r;
+		this.rgb_g_text.value = this.rgb_g_range.value = g;
+		this.rgb_b_text.value = this.rgb_b_range.value = b;
+	},
+
 	update_results: function()
 	{
 		this.rgb_result.value = "rgb(" + this.rgb_r_text.value + ", " + this.rgb_g_text.value + ", " + this.rgb_b_text.value + ")";
-		this.hex_result.value = "#" + this.hex_r_text.value + this.hex_g_text.value + this.hex_b_text.value;
+		this.hex_result.value = "#" + this.to_hex(this.hex_r_range.value) + this.to_hex(this.hex_g_range.value) + this.to_hex(this.hex_b_range.value);
 		this.hsl_result.value = "hsl(" + this.hsl_h_text.value + ", " + this.hsl_s_text.value + ", " + this.hsl_l_text.value + ")";
 		document.getElementById("main_div").style.backgroundColor = this.hex_result.value;
 	},
@@ -236,6 +398,14 @@ var App =
 		if (ret.length == 1)
 			ret = "0" + ret;
 		return (ret);
+	},
+
+	round: function(n)
+	{
+		if (n % 1 < 0.5)
+			return (Math.floor(n));
+		else
+			return (Math.floor(n) + 1);
 	},
 
 	select_elem: function(elem)
