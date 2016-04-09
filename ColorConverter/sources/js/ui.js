1b56bcdfd63384ed89c3eaf78b2df3a34ac58331
Kamipown <paul.delobbel@gmail.com>
2016-04-10 00:04:37 +0200

    Run mode handled : web browser or desktop

--- ColorConverter/sources/js/ui.js
+++ ColorConverter/sources/js/ui.js
@@ -1,52 +1,72 @@
 var Ui =
 {
+	mode: undefined,
 	nw: undefined,
 	window: undefined,
 	logo: undefined,
 
 	init: function()
 	{
-		this.nw = require("nw.gui");
-		this.window = nw.Window.get();
+		var self = this;
+		this.mode = 1;
+		try
+		{
+			this.nw = require("nw.gui");
+			this.window = nw.Window.get();
+			document.getElementById("minimize").onclick = function()
+			{
+				self.window.minimize();
+			}
+
+			document.getElementById("close").onclick = function()
+			{
+				self.window.close();
+			}
+		}
+		catch (e)
+		{
+			this.mode = 0;
+		}
+		this.init_ui();
+	},
+
+	init_ui: function()
+	{
 		this.logo = document.getElementById("logo");
-
-		document.getElementById("minimize").onclick = function()
-		{
-			Ui.window.minimize();
-		}
-
-		document.getElementById("close").onclick = function()
-		{
-			Ui.window.close();
-		}
-
 		this.update_logo_color(1);
+		if (!this.mode)
+		{
+			document.getElementById("window").style.borderRadius = "0px";
+			document.getElementById("main_header").style.display = "none";
+			document.getElementById("main_div").style.height = "100%";
+		}
 	},
 
 	update_logo_color: function(c)
 	{
+		var self = this;
 		if (c == 1)
 		{
 			setTimeout(function()
 			{
-				Ui.logo.style.fill = "#55c";
-				Ui.update_logo_color(2);
+				self.logo.style.fill = "#55c";
+				self.update_logo_color(2);
 			}, 5000);
 		}
 		else if (c == 2)
 		{
 			setTimeout(function()
 			{
-				Ui.logo.style.fill = "#5c5";
-				Ui.update_logo_color(3);
+				self.logo.style.fill = "#5c5";
+				self.update_logo_color(3);
 			}, 5000);
 		}
 		else if (c == 3)
 		{
 			setTimeout(function()
 			{
-				Ui.logo.style.fill = "#c55";
-				Ui.update_logo_color(1);
+				self.logo.style.fill = "#c55";
+				self.update_logo_color(1);
 			}, 5000);
 		}
 	}
